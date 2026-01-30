bd5ad225abfc2fcf38c9078d9968c35f9075dda9
jalal447 <jalaludheen3113@gmail.com>
2026-01-30 16:06:12 +0530

    new migration

--- app/users/models.py
+++ app/users/models.py
@@ -7,7 +7,7 @@ from typing import List
 
 class User(Base):
     __tablename__ = "user"
-
+#asdfghjkl;
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
     hashed_password: Mapped[str] = mapped_column(String, nullable=False)
