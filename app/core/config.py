7491d258e899e154e2aea99ebc09343ad8ed2e98
jalal447 <jalaludheen3113@gmail.com>
2026-01-30 15:57:30 +0530

    new change

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
-    
+#hii
     POSTGRES_SERVER: str="localhost"
     POSTGRES_USER: str="postgres"
     POSTGRES_PASSWORD: str="admin"
